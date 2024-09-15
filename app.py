780301aaa21da99a59a312f826c7177ecbab2c47
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2024-09-15 16:12:24 -0300

    rendering add_book.html succesfuly now

--- app.py
+++ app.py
@@ -71,15 +71,14 @@ def add_book():
             flash("Author not found. Try to register a new author to our database.")
             return render_template("add_book.html")
 
-        authorship_id = db.execute(
-            "SELECT authorship_id FROM authorships WHERE book_id = ? AND author_id = ?",book_id[0]["book_id"], author_id[0]["author_id"]
-        )
+        try:
+            authorship_id = db.execute(
+                "SELECT authorship_id FROM authorships WHERE book_id = ? AND author_id = ?",book_id[0]["book_id"], author_id[0]["author_id"]
+            )[0]["authorship_id"]
 
-        if not len(authorship_id):
-            flash("Probably this book was not written by this author, please check and try again.")
-            return render_template("add_book.html")
-        
-        authorship_id = authorship_id[0]["authorship_id"]
+        except:
+            flash("Book and/or author not found. Try to register a new book/author to our database.")
+            return render_template("add_book.html", book_titles=book_titles, author_names=author_names)
 
         if action == "reading":
             db.execute(
